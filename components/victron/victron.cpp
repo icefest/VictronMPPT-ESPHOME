d26a3b0389231138c9367de327c8ad3296fc5d81
icefest <robert.springa@gmail.com>
2024-06-04 16:29:51 +1000

    Update victron.cpp
    added Blue Smart IP22 Charger 24|12

--- components/victron/victron.cpp
+++ components/victron/victron.cpp
@@ -586,6 +586,8 @@ static std::string device_type_text(int value) {
       return "Phoenix Inverter 48V 3000VA 230V";
     case 0xA30A:
       return "Blue Smart IP65 Charger 12|25";
+    case 0xA334:
+      return "Blue Smart IP22 Charger 24|12";
     case 0xA340:
       return "Phoenix Smart IP43 Charger 12|50 (1+1)";
     case 0xA341:
